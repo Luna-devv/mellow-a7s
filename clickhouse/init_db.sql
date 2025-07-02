572e69b21a88b9b242b67af25a6d380ddd7ac852
Luna <luni.siaa@gmail.com>
2025-07-02 17:22:16 +0200

    add actions & commands tablers

--- clickhouse/init_db.sql
+++ clickhouse/init_db.sql
@@ -8,3 +8,21 @@ CREATE TABLE IF NOT EXISTS events.tts (
 ) ENGINE = MergeTree()
 ORDER BY (timestamp, service_id)
 PARTITION BY toYYYYMM(timestamp);
+
+CREATE TABLE IF NOT EXISTS events.commands (
+    timestamp DateTime64(3) DEFAULT now64(3),
+    user_id String,
+    type LowCardinality(String),
+    name LowCardinality(String),
+) ENGINE = MergeTree()
+ORDER BY (timestamp, name)
+PARTITION BY toYYYYMM(timestamp);
+
+CREATE TABLE IF NOT EXISTS events.actions (
+    timestamp DateTime64(3) DEFAULT now64(3),
+    guild_id String,
+    type LowCardinality(String),
+    name LowCardinality(String),
+) ENGINE = MergeTree()
+ORDER BY (timestamp, name)
+PARTITION BY toYYYYMM(timestamp);
