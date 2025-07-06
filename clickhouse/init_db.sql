854c3ed671b9c1aadc4cac73fec2293136623a00
Luna <luni.siaa@gmail.com>
2025-07-06 21:08:46 +0200

    remove actions event type

--- clickhouse/init_db.sql
+++ clickhouse/init_db.sql
@@ -21,7 +21,6 @@ PARTITION BY toYYYYMM(timestamp);
 CREATE TABLE IF NOT EXISTS events.actions (
     timestamp DateTime64(3) DEFAULT now64(3),
     guild_id String,
-    type LowCardinality(String),
     name LowCardinality(String),
 ) ENGINE = MergeTree()
 ORDER BY (timestamp, name)
