37f29b7465753a15c745ab1b1d68ece36fd828f9
Luna Seemann <luna@wamellow.com>
2026-07-02 19:19:29 +0200

    feat: service id (#3)

--- clickhouse/init_db.sql
+++ clickhouse/init_db.sql
@@ -2,28 +2,30 @@ CREATE DATABASE IF NOT EXISTS events;
 
 CREATE TABLE IF NOT EXISTS events.tts (
     timestamp DateTime64(3) DEFAULT now64(3),
-    service_id String,
+    service_id LowCardinality(String),
     language LowCardinality(String),
     speaker LowCardinality(String),
     chars UInt16,
     transed Boolean DEFAULT false
 ) ENGINE = MergeTree()
-ORDER BY (timestamp, service_id)
+ORDER BY (service_id, timestamp)
 PARTITION BY toYYYYMM(timestamp);
 
 CREATE TABLE IF NOT EXISTS events.commands (
     timestamp DateTime64(3) DEFAULT now64(3),
+    service_id LowCardinality(String),
     user_id String,
     type LowCardinality(String),
-    name LowCardinality(String),
+    name LowCardinality(String)
 ) ENGINE = MergeTree()
-ORDER BY (timestamp, name)
+ORDER BY (service_id, name, timestamp)
 PARTITION BY toYYYYMM(timestamp);
 
 CREATE TABLE IF NOT EXISTS events.actions (
     timestamp DateTime64(3) DEFAULT now64(3),
+    service_id LowCardinality(String),
     guild_id String,
-    name LowCardinality(String),
+    name LowCardinality(String)
 ) ENGINE = MergeTree()
-ORDER BY (timestamp, name)
+ORDER BY (service_id, name, timestamp)
 PARTITION BY toYYYYMM(timestamp);
